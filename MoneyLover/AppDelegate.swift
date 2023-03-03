29ef406abf3bbf7717f5034b3388d760d0a625ad
xhiew <nxhieu21@gmail.com>
2023-03-03 15:10:40 +0700

    first commit

---
+++ MoneyLover/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  MoneyLover
+//
+//  Created by xhieu21 on 03/03/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.makeKeyAndVisible()
+    window?.backgroundColor = .systemBackground
+    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    return true
+  }
+}
+
---
+++ MoneyLover/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoneyLover
+//
+//  Created by xhieu21 on 03/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    
+  }
+
+}
+
