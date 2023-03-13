83f561b85d762653b49f456578f416090f1a8f78
xhiew <nxhieu21@gmail.com>
2023-03-13 12:50:21 +0700

    add icons and images

--- MoneyLover/AppDelegate.swift
+++ MoneyLover/AppDelegate.swift
@@ -17,8 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
     window?.makeKeyAndVisible()
     window?.backgroundColor = .systemBackground
-    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    window?.rootViewController = ViewController()
     return true
   }
+
 }
+
 
